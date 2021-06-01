e6495301833a421f833ddd7166aa795eba2f1b7e
Michael <uberigloo@gmail.com>
2021-06-02 00:00:53 +0300

    button added

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
-
+import json
+import time
 
 url = 'https://api.telegram.org/bot1833113848:AAFEJ2jhJn1nBw2D-pg4Wo61IQSJZtjVwek/'
 
@@ -11,16 +12,28 @@ def get_updates_json(request):
 def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
+    print(total_updates)
     return results[total_updates]
 
 def get_chat_id(update):
     chat_id = update['message']['chat']['id']
     return chat_id
 
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
+def get_last_message(update):
+    last_chat_text = update['message']['text']
+    return last_chat_text
+
+def send_message(chat, text):
+    params = {'chat_id': chat, 'text': text,}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+def send_keyboard(chat, text):
+    reply_markup = {"keyboard": [["1", "2"], ["3"]], "one_time_keyboard": True,
+                "resize_keyboard": True}
+    params = {'chat_id': chat, 'text': text, 'reply_markup' : json.dumps(reply_markup)}
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
 chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'test message')
+last_chat_text = get_last_message(last_update(get_updates_json(url)))
