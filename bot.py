da46c490659f5e75b50055a5a65d0394e9bb7e4b
Michael <uberigloo@gmail.com>
2021-06-02 18:36:57 +0300

    refactor v1

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ def get_updates_json(request):
 def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
-    print(total_updates)
     return results[total_updates]
 
 
@@ -40,18 +39,21 @@ def send_keyboard(chat, text, keyboard):
                     "resize_keyboard": True,
                     "selective": True}
 
-    params = {'chat_id': chat, 'text': text, 'reply_markup': json.dumps(reply_markup)}
+    params = {'chat_id': chat, 'text': text,
+              'reply_markup': json.dumps(reply_markup)}
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
 
 chat_id = get_chat_id(last_update(get_updates_json(url)))
-last_chat_text = get_last_message(last_update(get_updates_json(url)))
+# last_chat_text = get_last_message(last_update(get_updates_json(url)))
 
-keyboard_alcohol = [["Вино", "Пиво", "Крепкие напитки", "Мартини/Текила"]]
+keyboard_alcohol = [["Вино", "Пиво", "Крепкие напитки", "Мартини"]]
 keyboard_state = [["Мужчина", "Женщина"]]
-keyboard_amount = [["100","250","500"],["750","1000","1 литр и больше"]]
-keyboard_weight = [["50 ", "60","70"],["80","90","100"]]
+keyboard_amount = [["100", "250", "500"], ["750", "1000", "1 литр и больше"]]
+keyboard_weight = [["50 ", "60", "70"], ["80", "90", "100"]]
+keyboard_init = [["start", "help"]]
+
 
 def main():
     send_keyboard(chat_id, 'Укажите ваш пол:', keyboard_state)
@@ -61,40 +63,45 @@ def main():
     send_keyboard(chat_id, 'Что вы упортебляли?', keyboard_alcohol)
     time.sleep(5)
     alco = get_last_message(last_update(get_updates_json(url)))
+
     send_message(chat_id, ' Вижу вам нравится {}'.format(alco))
     time.sleep(1)
 
     send_keyboard(chat_id, 'Сколько грамм?', keyboard_amount)
     time.sleep(5)
-    grams = get_last_message(last_update(get_updates_json(url)))
+    grams = int(get_last_message(last_update(get_updates_json(url))))
 
-    send_keyboard(chat_id, 'Укажите , пожалуйтса , ваш вес',keyboard_weight)
+    send_keyboard(chat_id, 'Укажите , пожалуйтса , ваш вес', keyboard_weight)
     time.sleep(5)
-    weight = get_last_message(last_update(get_updates_json(url)))
+    weight = int(get_last_message(last_update(get_updates_json(url))))
 
-    if alco == 'Вино':
-        spirt = int(grams)/10
-    if alco == "Пиво":
-        spirt = int(grams)/20
-    if alco == "Крепкие напитки":
-        spirt = int(grams)/(100/40)
-    if alco == "Мартини/Текила":
-        spirt = int(grams)/(100/12)
-
-
-    if state == "Мужчина":
-        kf = 0.7
-    if state == "Женщина":
-        kf = 0.6
-
-
-    promile = round(spirt/(int(weight)*kf),2)
+    alcohol_gradus = {'Вино': 12, "Пиво": 5,
+                      "Крепкие напитки": 40, "Мартини/Текила": 13}
+    spirt = grams*(alcohol_gradus[alco]/100)
+    states = {'Мужчина': 0.7, 'Женщина': 0.6}
+    kf = states[state]
+    promile = round(spirt/(weight*kf), 2)
     send_message(chat_id, 'calculating')
     time.sleep(3)
     send_message(chat_id, 'promile is {}'.format(promile))
-    
+
     if promile < 0.2:
-        send_message(chat_id,"Садитесь за руль , но будьте аккуратны")
-    else :
+        send_message(chat_id, "Садитесь за руль , но будьте аккуратны")
+    else:
         send_message(chat_id, 'Пьяний за кермо не сiдай')
-main()
+
+
+while True:
+    send_message(
+        chat_id, 'Привет , бот показывает можно ли вам садиться за руль.Просто введите свои парметры')
+    send_keyboard(chat_id, 'choose', keyboard_init)
+    time.sleep(2)
+    last_chat_text = get_last_message(last_update(get_updates_json(url)))
+    print(last_chat_text)
+    time.sleep(4)
+    if last_chat_text == 'start':
+        main()
+    else:
+        send_message(
+            chat_id, "You can contact developer on github if smth not working")
+
