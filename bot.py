05e54c145d4bd11992fb6213c75057971e2adecf
Michael <uberigloo@gmail.com>
2021-06-03 23:42:59 +0300

    variables names changed, readability improved

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import time
-import threading
 
 
 token = json.load(open("settings.json", "r"))["TOKEN"]
@@ -9,55 +8,51 @@ url = 'https://api.telegram.org/bot{}/'.format(token)
 
 
 def get_updates_json(request, update_id=None):
+    params = {'allowed_updates': 'message', 'offset': str(update_id)}
     if update_id != None:
-        response = requests.get(request + 'getUpdates?allowed_updates=message&offset='+str(update_id))
+        response = requests.get(request + 'getUpdates', data=params)
     else:
-        response = requests.get(request + 'getUpdates?allowed_updates=message')
+        response = requests.get(request + 'getUpdates', data=params['allowed_updates'])
+
     return response.json()
 
 
 def last_update(data):
-    if 'result' in data.keys():
-        results = data['result']
-        return results[-1]
-    # else:
-        
+    results = data['result']
+    return results[-1]
 
-
-    
-    
 
 def get_chat_id(update):
     chat_id = update['message']['chat']['id']
     return chat_id
 
+def get_last_message(update):
+    last_chat_text = update['message']['text']
+    return last_chat_text
 
 chat_id = get_chat_id(last_update(get_updates_json(url)))
+last_chat_id = chat_id
 
 
 def check_result(data):
+    global last_chat_id
     last_update_id = data['update_id']
     cycle_update = {'update_id': 0}
     while True:
-        cycle_update = last_update(get_updates_json(url, cycle_update['update_id']))
-        if cycle_update == None :
-            print(cycle_update)
+        cycle_update = last_update(
+            get_updates_json(url, cycle_update['update_id']))
+        if cycle_update == None:
             time.sleep(0.5)
             continue
+        print(cycle_update['update_id'])
         if last_update_id == cycle_update['update_id']:
             time.sleep(0.5)
         else:
             if chat_id == cycle_update['message']['chat']['id']:
                 return cycle_update
             else:
-                send_message(chat_id, 'bot is busy')
-
-
-
-def get_last_message(update):
-    last_chat_text = update['message']['text']
-    return last_chat_text
-
+                last_chat_id = cycle_update['message']['chat']['id']
+        
 
 def send_message(chat, text):
     params = {'chat_id': chat, 'text': text, }
@@ -77,20 +72,17 @@ def send_keyboard(chat, text, keyboard):
     return response
 
 
-# last_chat_text = get_last_message(last_update(get_updates_json(url)))
-
 keyboard_alcohol = [["Вино", "Пиво", "Крепкие напитки", "Мартини"]]
-keyboard_state = [["Мужчина", "Женщина"]]
-keyboard_amount = [["100", "250", "500"], ["750", "1000", "1 литр и больше"]]
+keyboard_gender = [["Мужчина", "Женщина"]]
+keyboard_grams = [["50", "100", "250"], ["333", "500", "1000"]]
 keyboard_weight = [["50 ", "60", "70"], ["80", "90", "100"]]
 
 
-def get_state():
-    send_keyboard(chat_id, 'Укажите ваш пол:', keyboard_state)
-    state = check_result(last_update(get_updates_json(url)))['message']['text']
-    # state = get_last_message(last_update(get_updates_json(url)))
-    print('state = {}'.format(state))
-    return state
+def get_gender():
+    send_keyboard(chat_id, 'Укажите ваш пол:', keyboard_gender)
+    gender = check_result(last_update(get_updates_json(url)))['message']['text']
+    print('gender = {}'.format(gender))
+    return gender
 
 
 def get_drink():
@@ -101,8 +93,8 @@ def get_drink():
     return alco
 
 
-def get_amount():
-    send_keyboard(chat_id, 'Сколько грамм?', keyboard_amount)
+def get_grams():
+    send_keyboard(chat_id, 'Сколько грамм?', keyboard_grams)
     grams = int(check_result(last_update(get_updates_json(url)))
                 ['message']['text'])
     print('grams = {}'.format(grams))
@@ -117,13 +109,13 @@ def get_weight():
     return weight
 
 
-def calculate_promile(grams, alco, state, weight):
+def calculate_promile(grams, alco, gender, weight):
     alcohol_gradus = {'Вино': 12, "Пиво": 5,
                       "Крепкие напитки": 40, "Мартини": 13}
-    states = {'Мужчина': 0.7, 'Женщина': 0.6}
-    spirt = grams*(alcohol_gradus[alco]/100)
-    kf = states[state]
-    promile = round(spirt/(weight*kf), 2)
+    genders = {'Мужчина': 0.7, 'Женщина': 0.6}
+    alcohol_amount = grams*(alcohol_gradus[alco]/100)
+    kf = genders[gender]
+    promile = round(alcohol_amount/(weight*kf), 2)
     return promile
 
 
@@ -140,16 +132,17 @@ def send_answer(promile):
 
 
 def main():
-    state = get_state()
+    gender = get_gender()
     alco = get_drink()
-    grams = get_amount()
+    grams = get_grams()
     weight = get_weight()
-    promile = calculate_promile(grams, alco, state, weight)
+    promile = calculate_promile(grams, alco, gender, weight)
     send_answer(promile)
 
 
 while True:
+
     send_message(chat_id, 'Привет , бот показывает можно ли вам садиться\
-         за руль . Просто введите свои парметры')
+    за руль . Просто введите свои парметры')
     main()
-    break
+    chat_id = last_chat_id
